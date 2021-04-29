a810774c143b5dcfa6bd478dd64499dce20eee74
Adam Butterworth <abutterworth@edx.org>
2021-04-29 10:02:32 -0400

    feat: add theme js

---
+++ paragon/theme.js
@@ -0,0 +1,10 @@
+// The theme.js for Paragon. The structure of this object follows the
+// theme specification found here: https://system-ui.com/theme/. Theme values
+// not defined here will fallback to their Paragon defaults.
+
+const theme = {
+
+};
+
+export default theme;
+
